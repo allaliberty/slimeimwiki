764551d843834946d33ccfb8b8701e8cd3b75252
Seikirin <81483357+Seikirin@users.noreply.github.com>
2022-07-25 14:23:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -891,7 +891,7 @@ function UpdatePage(category) {
             })
             let currentbanner
             Object.keys(EventsData).forEach((key) =>  { 
-                if (EventsData[key].Type == "Recruit" && currentbanner == null && EventsData[key].Banner != undefined && EventsData[key].Banner != null)
+                if (EventsData[key].Type == "Recruit" && currentbanner == null && ("Banner" in EventsData[key]) && EventsData[key].Banner.length > 0)
                 {
                     currentbanner = key;
                     return;
